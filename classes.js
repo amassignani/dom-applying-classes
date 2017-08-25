17f5e0c966b7d2fee65922aebc4e1c9c2e638dc7
amassignani <feelsgreat2016@gmail.com>
2017-08-25 15:40:05 +1200

    adding pink dot

--- classes.js
+++ classes.js
@@ -7,6 +7,7 @@ function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
   one()
   two()
+  three()
 
   // Your turn! Create a new function called `two`, then call it from here.
 }
@@ -18,12 +19,17 @@ function one () {
   // Next, we apply a new CSS class to it:
   one.classList.add('blue');
 }
+// CREATE FUNCTION two HERE
+
 function two () {
   var two = document.getElementById('two')
   two.classList.add('green');
 }
-// CREATE FUNCTION two HERE
 
 // CREATE FUNCTION three HERE
+function three () {
+  var three = document.getElementById('three')
+  three.classList.add('pink');
+}
 
 // CREATE FUNCTION makeVisible HERE
